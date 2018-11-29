30c08e72b2cc7ab52609957e91ea629eff47aadf
WoojinNam <madwind7@gmail.com>
2018-11-29 12:19:51 +0900

    Dockerfile modify

--- extra/docker/start.sh
+++ extra/docker/start.sh
@@ -2,5 +2,8 @@
 echo Starting MongoDB
 mongod --fork --logpath /home/volutility/mongodb.log --dbpath /home/volutility/dbpath/
 sleep 5
+echo Django migrate
+python manage.py migrate
+sleep 5
 echo Starting VolUtility
 python manage.py runserver 0.0.0.0:8080
