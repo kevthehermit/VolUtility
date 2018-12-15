3f1414ef1ce9483741dd4052c1df36cc978f6b14
WoojinNam <madwind7@gmail.com>
2018-12-15 20:12:32 +0900

    Modify start.sh
    Add Virustotal API KEY

--- extra/docker/start.sh
+++ extra/docker/start.sh
@@ -5,5 +5,9 @@ sleep 5
 echo Django migrate
 python manage.py migrate
 sleep 5
+if [ ! -z "$VT_APIKEY" ]; \
+        then echo "VT API Setting: $VT_APIKEY"; sed -i -e "s/api_key = None/api_key = $VT_APIKEY/g" ~/.volutility.conf; \
+    fi
+sleep 5
 echo Starting VolUtility
 python manage.py runserver 0.0.0.0:8080
